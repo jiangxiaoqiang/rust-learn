74af19901b72e69106e62a93c66b8073298e12d8
jiang qiang <jiangtingqiang@gmail.com>
2021-09-22 16:39:05 +0800

    [feature] tweak

--- src/main.rs
+++ src/main.rs
@@ -18,16 +18,12 @@ use crate::api_response::ApiResponse;
 fn index() -> &'static str {
     use crate::schema::favorites::dsl::*;
     let connection = config::establish_connection();
-    let input_song_id = record.id;
+    //let input_song_id = record.id;
     let post = diesel::update(favorites.find(id))
         .set(play_count.eq(200))
         .get_result::<QueryFavorites>(&connection)
         .expect(&format!("Unable to find post {}", input_song_id));
-    let res = ApiResponse {
-        result: post,
-        ..Default::default()
-    };
-    let response_json = serde_json::to_string(&res).unwrap();
+
     return "ok";
 }
 
