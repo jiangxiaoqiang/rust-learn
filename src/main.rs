5c53784dac3f49d30c1f9c2fb4eb1273ff56393a
jiang qiang <jiangtingqiang@gmail.com>
2021-09-30 19:44:22 +0800

    [feature] remove unused code

--- src/main.rs
+++ src/main.rs
@@ -9,31 +9,11 @@ mod config;
 mod api_response;
 mod login_user_info;
 
-use crate::diesel::prelude::*;
-use diesel::pg::expression::dsl::any;
-use rocket::serde::Deserialize;
-use rocket::serde::Serialize;
-use crate::api_response::ApiResponse;
 use crate::login_user_info::LoginUserInfo;
 
 #[get("/<id>")]
 fn index(id:&str,loginUserInfo: LoginUserInfo) -> &'static str {
     return "ok";
-}
-
-#[derive(Debug, PartialEq, Eq, Deserialize,Serialize)]
-#[allow(non_snake_case)]
-pub struct ArtistResponse {
-    pub id: i64,
-    pub name: String
-}
-
-#[derive(Debug, PartialEq, Eq, Deserialize,Serialize)]
-#[allow(non_snake_case)]
-pub struct MusicResponse {
-    pub id: i64,
-    pub title: String,
-    pub artist: Vec<ArtistResponse>
 }
 
 #[launch]
