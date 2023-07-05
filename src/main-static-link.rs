2ccce7309f3ee839f882f76bfa8a8fd884e5f194
JiangXiaoqiang <jiangtingqiang@gmail.com>
2023-07-06 04:07:33 +0800

    feat: tweak

---
+++ src/main-static-link.rs
@@ -0,0 +1,18 @@
+extern crate openssl;
+#[macro_use]
+extern crate diesel;
+
+use diesel::QueryDsl;
+use crate::diesel::RunQueryDsl;
+use crate::{model::diesel::alt::custom_alt_models::AltTag, common::database::get_connection};
+
+pub mod model;
+pub mod common;
+
+fn main() {
+    use crate::model::diesel::alt::alt_schema::alt_tag as cv_work_table;
+    let query = cv_work_table::table.into_boxed::<diesel::pg::Pg>();
+    let _cvs = query
+        .load::<AltTag>(&mut get_connection())
+        .expect("error get tags list");
+}
--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,7 @@
-extern crate openssl;
-#[macro_use]
-extern crate diesel;
-
-use diesel::QueryDsl;
-use crate::diesel::RunQueryDsl;
-use crate::{model::diesel::alt::custom_alt_models::AltTag, common::database::get_connection};
-
-pub mod model;
-pub mod common;
+use std::env;
 
 fn main() {
-    use crate::model::diesel::alt::alt_schema::alt_tag as cv_work_table;
-    let query = cv_work_table::table.into_boxed::<diesel::pg::Pg>();
-    let _cvs = query
-        .load::<AltTag>(&mut get_connection())
-        .expect("error get tags list");
+    for (key, value) in env::vars() {
+        println!("{}: {}", key, value);
+    }
 }
